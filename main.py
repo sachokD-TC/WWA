4caa13afc8698c5daaaf59abe4bc4589107eda17
SachokD-TC <Rythm1977>
2017-09-24 21:24:27 +0300

    add new actors pictures, add control for them, rename menu to main

---
+++ main.py
@@ -0,0 +1,107 @@
+import pygame
+import pytmx
+from pytmx.util_pygame import load_pygame
+from wwa import Wwa
+
+pygame.init()
+
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(0, 0, 0), font=None, font_size=70,
+                 font_color=(15, 12, 0)):
+        self.screen = screen
+        self.back_img = pygame.Surface((42 * 32, 42 * 32))
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+
+        self.items = items
+        self.font = pygame.font.SysFont(font, font_size)
+        self.font_color = font_color
+        self.menu_image = pygame.image.load('pic/menu.png')
+        self.back_map = load_pygame("map//menu_background.tmx")
+
+        self.items = []
+        for index, item in enumerate(items):
+            label = self.font.render(item, 1, font_color)
+
+            width = label.get_rect().width
+            height = label.get_rect().height
+
+            posx = (self.scr_width / 2) - (width / 2)
+            t_h = len(items) * height
+            posy = (self.scr_height / 2 ) - (t_h / 2) + (index * height)
+
+            self.items.append([item, label, (width, height), (posx, posy)])
+
+    def redraw_menu(self, new=None):
+        for name, label, (width, height), (posx, posy) in self.items:
+            if new is not None and new == name:
+                new = self.font.render(name, 1, (255, 255, 0))
+                self.screen.blit(new, (posx, posy + 45))
+            else:
+                self.screen.blit(label, (posx, posy + 45))
+
+
+    def run(self):
+        self.redraw_map()
+        screen.blit(self.back_img, (0, 0))
+        screen.blit(self.menu_image, (190, 100))
+        self.redraw_menu()
+        pygame.display.update()
+
+        mainloop = True
+        xmove = 0
+        inc = -1
+        while mainloop:
+            self.clock.tick(50)
+            posm =pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+            new = None
+            for name, label, (width, height), (posx, posy) in self.items:
+                if posm[0] > posx and posm[0] < posx + width and posm[1] > posy and posm[1] < posy + height:
+                    new = name
+                    pygame.display.update()
+                    if click[0] and name == 'Quit':
+                        mainloop = False
+                    if click[0] and name == 'Start':
+                        Wwa(1, True, False)
+                    if click[0] and name == 'About':
+                        print('About - is clicked')
+
+            screen.blit(self.back_img, (xmove, 0))
+            if xmove <= -400:
+                inc = 1
+            if xmove > 0:
+                inc =-1
+            xmove += inc;
+
+            screen.blit(self.menu_image, (190, 100))
+            self.redraw_menu(new)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mainloop = False
+
+    def redraw_map(self):
+        for layer in self.back_map.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x in range(0, 40):
+                    for y in range(0, 40):
+                      image = self.back_map.get_tile_image(x, y, 0)
+                      if image != None:
+                        self.back_img.blit(image, (32 * x, 32 * y))
+
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((800, 800), 0, 32)
+    icon = pygame.image.load('pic/haticon.png')
+    pygame.display.set_icon(icon)
+
+    menu_items = ('Start', 'About', 'Quit')
+
+    pygame.display.set_caption('Wild West Adventure')
+    gm = GameMenu(screen, menu_items, (175,159,75),'JOKERMAN')
+    gm.run()
--- players/cowboy.py
+++ players/cowboy.py
@@ -7,17 +7,21 @@ class Cowboy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('actors/right.png')
         self.rect = self.image.get_rect()
-        self.actions = {const.LEFT: "left.png",
-                        const.RIGHT: "right.png",
-                        const.DOWN: "front.png",
-                        const.UP: "back.png",
-                        const.REST: "front.png"}
+        self.actions = {const.LEFT: ["left.png", "left_1.png", "left.png", "left_2.png"],
+                        const.RIGHT: ["right.png", "right_1.png", "right.png", "right_2.png"],
+                        const.DOWN: ["front.png", "front_1.png", "front.png", "front_2.png"],
+                        const.UP: ["back.png", "back_1.png","back.png", "back_2.png"],
+                        const.REST: ["front.png"]}
         self.action = "left"
         self.rect.x = 50
         self.rect.y = 50
+        self.ind = 0
+        self.move_in_row = 0;
+        self.prev_move = const.REST
         self.pos_x = self.rect.x - 40
         self.pos_y = self.rect.y - 40
-        self.movement_dict = {const.LEFT: (-2, 0), const.RIGHT: (2, 0), const.DOWN: (0, 2), const.UP: (0, -2), const.REST: (0, 0)}
+        self.movement_dict = {const.LEFT: (-2, 0), const.RIGHT: (2, 0), const.DOWN: (0, 2), const.UP: (0, -2),
+                              const.REST: (0, 0)}
         self.movement = 'rest'
 
     def update(self, event):
@@ -39,11 +43,18 @@ class Cowboy(pygame.sprite.Sprite):
                     self.movement = const.UP
             elif event.type == pygame.KEYUP:
                 self.movement = const.REST
+            self.inc_ind(self.movement)
             self.rect.x += self.movement_dict[self.movement][0]
             self.rect.y += self.movement_dict[self.movement][1]
             self.pos_x -= self.movement_dict[self.movement][0]
             self.pos_y -= self.movement_dict[self.movement][1]
-            self.image = pygame.image.load('actors/' + self.actions[self.movement])
+            self.image = pygame.image.load('actors/' + self.actions[self.movement][self.ind])
+
+    def inc_ind(self, move):
+        if self.ind >= len(self.actions[move]) - 1:
+            self.ind = 0
+        else:
+            self.ind += 1
 
     def draw(self, display):
         display.blit(self.image, self.rect)
